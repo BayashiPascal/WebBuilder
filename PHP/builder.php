e63909cc4056619126832e06cb5ee444b9cb1d1e
Pascal Baillehache <30551642+BayashiPascal@users.noreply.github.com>
2018-02-25 18:15:16 +0900

    Remove the test of login for security reasons

--- PHP/builder.php
+++ PHP/builder.php
@@ -553,12 +553,7 @@ function BuildLogin($theWB) {
   $block .= 'Login access';
   $block .= '</div>';
   $block .= '<div class="divTileMainTxt">';
-  $block .= 'The access to a page can be restricted to logged in user. A new user can be created with ' . FormatCode('$theWB->CreateUserLogin($login, $passwd);') . '<br>A login plate can be created with <br>' . FormatCode('$theWB->BuildDivLogger($submitTo);') . '<br>The current logged in status can be checked with <br>' . FormatCode('if ($theWB->IsLogged() == false) {<br>echo $theWB->BuildDivLogger("index.php");<br>} else {<br>echo "you are logged in";<br>}') . '<br>When logged in a link to logout is automatically added to the footer.<br>You can try yourself below with the login "WebBuilder" and password "WebBuilder".<br><br>';
-  if ($theWB->IsLogged() == false) {
-    $block .= $theWB->BuildDivLogger("index.php");
-  } else {
-    $block .= "you are logged in";
-  }
+  $block .= 'The access to a page can be restricted to logged in user. A new user can be created with ' . FormatCode('$theWB->CreateUserLogin($login, $passwd);') . '<br>A login plate can be created with <br>' . FormatCode('$theWB->BuildDivLogger($submitTo);') . '<br>The current logged in status can be checked with <br>' . FormatCode('if ($theWB->IsLogged() == false) {<br>echo $theWB->BuildDivLogger("index.php");<br>} else {<br>echo "you are logged in";<br>}') . '<br>When logged in a link to logout is automatically added to the footer.<br><br>';
   $block .= '</div>';
   return $block;
 }
