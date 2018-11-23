171b6b037ab3ef6f98f44a0cbdfef7d50e9b134c
BayashiPascal <bayashipascal@gmail.com>
2018-11-23 11:36:33 +0900

    Ensure https connection

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
   // ------------------ index.php ---------------------
   // Start the PHP session
   session_start();
+  // Require https
+  if ($_SERVER['HTTPS'] != "on") {
+    $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+    header("Location: $url");
+    exit;
+  }
   // Include the WebBuilder
   require("PHP/WebBuilder.php");
   // Process POST values
